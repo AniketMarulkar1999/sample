2ffc86ecf3dd7bc043f67c2bb79050188a766ed0
AniketMarulkar1999 <73399700+AniketMarulkar1999@users.noreply.github.com>
2021-03-02 17:19:34 +0530

    Add files via upload

---
+++ ViewModels/SelectedProductDialogViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Prism.Commands;
+using Product_Inventory.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Product_Inventory.ViewModels
+{
+    public class SelectedProductDialogViewModel
+    {
+        public static bool IsDataValidFlag;
+        public SelectedProductDialogViewModel()
+        { 
+        }
+
+        public bool ValidateData()
+        {
+            if (Product.ProductIdFlag == true && Product.ProductNameFlag == true && Product.ProductQtyFlag == true && Product.ProductPriceFlag == true)
+            {
+                IsDataValidFlag = true;
+                return IsDataValidFlag;
+            }
+            else
+            {
+                IsDataValidFlag = false;
+                return IsDataValidFlag;
+            }
+        }
+    }
+}
