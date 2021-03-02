9e6eedf879217e7a23643e5e12f35d9a58298e44
AniketMarulkar1999 <73399700+AniketMarulkar1999@users.noreply.github.com>
2021-03-02 16:52:51 +0530

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Prism.Ioc;
+using Product_Inventory.Dialogs;
+using Product_Inventory.Views;
+using System.Windows;
+using Prism.Mvvm;
+
+namespace Product_Inventory
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        App()
+        {
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("gr-GR");
+            new System.Globalization.CultureInfo("en");
+        }
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterDialog<Dialogs.MessageDialog, MessageDialogViewModel>();
+            
+        }
+
+        
+    }
+}
