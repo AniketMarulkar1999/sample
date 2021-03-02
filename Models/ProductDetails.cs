20290ff298234425a315f78cba835b80726c6543
AniketMarulkar1999 <73399700+AniketMarulkar1999@users.noreply.github.com>
2021-03-02 17:22:24 +0530

    Add files via upload

---
+++ Models/ProductDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Xml.Serialization;
+
+
+namespace Product_Inventory.Models
+{
+    [Serializable()]
+    [XmlRoot("ProductDetails")] 
+    public class ProductDetails
+    {
+        [XmlElement("Product")]
+        public List<Product> Product { get; set; }
+        //public Product product { get; set; }
+    }
+}
